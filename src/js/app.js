a171b2c803b27e38258680815f06b14f2d6390d7
NadezhdaJS <94574119+NadezhdaJS@users.noreply.github.com>
2022-09-05 17:13:59 +0400

    feat: get a random gif

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,7 @@
+let btn = document.querySelector(".btn");
+let gif = document.querySelector(".gif");
 
-
+btn.addEventListener("click", function () {
+  let randomGif = Math.trunc(Math.random() * 46 + 1);
+  gif.src = `img/${randomGif}.gif`;
+});
